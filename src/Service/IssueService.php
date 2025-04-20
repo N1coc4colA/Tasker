0a1761db43f14f0ed1e7d549205fc7b035004bdb
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-04-20 13:39:05 +0200

    feat: Add service repo

---
+++ src/Service/IssueService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service;
+
+use App\Enum\IssueStatus;
+use App\Repository\IssueRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class IssueService
+{
+    public function __construct(
+        private readonly IssueRepository $repo,
+        private readonly Security $security
+    )
+    {
+    }
+
+    public function getInProgressIssues(): array
+    {
+        return $this->getIssuesByStatus([IssueStatus::READY]);
+    }
+
+    public function getReadyIssues(): array
+    {
+        return $this->getIssuesByStatus([IssueStatus::IN_DEVELOPMENT, IssueStatus::IN_REVIEW]);
+    }
+
+    public function getResolvedIssues(): array
+    {
+        return $this->getIssuesByStatus([IssueStatus::RESOLVED]);
+    }
+
+    public function getIssuesByStatus(array $statuses): array
+    {
+        $user = $this->security->getUser();
+
+        return $user
+            ->getSelectedProject()
+            ->getIssues()
+            ->filter(fn($issue) => in_array($issue->getStatus(), $statuses))
+            ->toArray();
+    }
+}
